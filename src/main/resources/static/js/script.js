30e114d5fe2e8c3ca764ff2af3b0b41d42b5f29d
Mustaqim <mustaqim@apptware.com>
2021-01-24 17:31:54 +0530

    search functionality created

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,3 +10,36 @@ const toggleSidebar = () =>{
        $(".content").css("margin-left", "20%");
                }
       };
+
+      const search = () => {
+                   // console.log("searching..");
+
+
+                  let query = $("#search-input").val();
+
+                  if(query==""){
+                    $(".search-result").hide();
+                  }else{
+                    //searching
+                    console.log(query); //it show on console
+                    //sending request to server
+                    let url = `http://localhost:8080/search/${query}`;
+                    fetch(url).then((response) => {
+                      return response.json();
+                    }).then((data) => {
+
+                    //data...
+                    console.log(data);
+
+                    let text = `<div class='list-group'>`;
+
+                    data.forEach((contact) => {
+
+                       text+= `<a href='/user/${contact.cId}/contact' class='list-group-item list-group-item-action'> ${contact.firstName} </a>`;
+                    });
+                    text+= `</div>`;
+                    $(".search-result").html(text);
+                     $(".search-result").show();
+                    });
+                }
+               };
